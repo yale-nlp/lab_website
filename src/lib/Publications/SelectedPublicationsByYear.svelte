433b15f2e8546ef8cf3a1f17b02c00675ca3744a
yilunzhao <yilun.zhao@yale.edu>
2023-08-13 22:29:59 -0400

    add filter feature for publication page

--- src/lib/Publications/SelectedPublicationsByYear.svelte
+++ src/lib/Publications/SelectedPublicationsByYear.svelte
@@ -1,56 +1,74 @@
 <script lang="ts">
-import type {
-    Publication as PublicationInterface, 
-    Publications as PublicationsInterface, 
-    PublicationsGroupedByYear
-} from '$lib/types'
-export let publications: PublicationsInterface;
+    import type {
+        Publication as PublicationInterface,
+        Publications as PublicationsInterface,
+        PublicationsGroupedByYear
+    } from '$lib/types';
 
-$: years = {} as PublicationsGroupedByYear
+    export let publications: PublicationsInterface;
 
-$: publications.forEach((pub:PublicationInterface) => {
-    let year = String(pub?.year as number)
-    
-    if (!(year in years)) {
-        years[year] = [] as PublicationsInterface;
+    // Extracting all unique types
+    $: types = Array.from(new Set(publications.map((pub: PublicationInterface) => pub.type)));
+
+    let selectedType = "";
+
+    // Initialize years directly
+    let years: PublicationsGroupedByYear = {};
+
+    $: {
+        // Reset years for each change of publications or selectedType
+        years = {};
+
+        publications.forEach((pub: PublicationInterface) => {
+            if (selectedType && pub.type !== selectedType) return;
+
+            let year = String(pub?.year as number);
+
+            if (!(year in years)) {
+                years[year] = [] as PublicationsInterface;
+            }
+
+            years[year].push(pub);
+        });
     }
-    
-    years[year].push(pub)
-            
-});
 </script>
 
-<div class="">            
+
+<div class="">
+    <label for="paperType">Filter by Type:</label>
+    <select id="paperType" bind:value={selectedType}>
+        <option value="">All</option>
+        {#each types as type}
+            <option value={type}>{type}</option>
+        {/each}
+    </select>
+
     {#each Object.entries(years).reverse() as [year, pubs]}
-    <div>
-        <div class="grid grid-flow-col auto-cols-auto py-4">
-            <div class="text-4xl font-extralight col-auto">
-                <div class="sticky top-0 ">
-                    {year}
-                </div>                        
-            </div>
-            <div class="ml-16 col-auto">
-                {#each pubs as pub}
-                <div class="pb-4">
-                    <div class="card-title break-word">
-                        {#if pub?.href}
-                        <a class="link" href={pub?.href}>
-                            {pub?.title}
-                        </a>   
-                        {:else}
-                            {pub?.title}
-                        {/if}
+        <div>
+            <div class="grid grid-flow-col auto-cols-auto py-4">
+                <div class="text-4xl font-extralight col-auto">
+                    <div class="sticky top-0 ">
+                        {year}
                     </div>
-                    <span>
-                        {@html pub.makeAuthorString()}                                
-                    </span>
-                    <span>
-                        {@html pub.makePublicationString()}
-                    </span>   
                 </div>
-                {/each}
-            </div>  
+                <div class="ml-16 col-auto">
+                    {#each pubs as pub}
+                        <div class="pb-4">
+                            <div class="card-title break-word">
+                                {#if pub?.href}
+                                    <a class="link" href={pub?.href}>
+                                        {pub?.title}
+                                    </a>
+                                {:else}
+                                    {pub?.title}
+                                {/if}
+                            </div>
+                            <span>{@html pub.makeAuthorString()}</span>
+                            <span>{@html pub.makePublicationString()}</span>
+                        </div>
+                    {/each}
+                </div>
+            </div>
         </div>
-    </div>
     {/each}
 </div>
