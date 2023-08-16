ff33ec3cacc741a60839de3f27fafc9ea9311846
yilunzhao <yilun.zhao@yale.edu>
2023-08-16 15:46:19 -0400

    fix the bug in member page when mouse moving too fast

--- src/lib/MemberInfo.svelte
+++ src/lib/MemberInfo.svelte
@@ -17,60 +17,65 @@
     let showDetails = false;
 </script>
 
+
 <style>
     .card {
         position: relative;
-        width: 100%; 
+        width: 100%;
         height: 210pt;
         padding: 5px;
         border-radius: 8px;
-        /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); */
         text-align: center;
         overflow: hidden;
-        display: flex; /* Add flex layout */
-        flex-direction: column; /* Stack children vertically */
+        display: flex;
+        flex-direction: column;
     }
 
     .details-container {
-        background-color: rgb(255, 140, 0);
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255, 140, 0, 0.95);
+        z-index: 10;
         color: white;
-        height: 205pt;
         padding: 10px;
         opacity: 0;
         transition: all 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55);
-        display: flex; /* Add flex layout */
-        flex-direction: column; /* Stack children vertically */
+        display: flex;
+        flex-direction: column;
+
+        transform: translateY(-100%); 
+        max-height: 0;
+        overflow: hidden;
     }
 
     .details-container.active {
         opacity: 1;
+        transform: translateY(0);
+        max-height: 210pt;
     }
 
-    .details-container h2 {
+    .details-container h2, .card-body h2 {
         text-align: center;
         font-size: large;
         font-weight: 600;
     }
 
-    .details-container p {
+    .details-container p, .card-body p {
         text-align: center;
         font-size: small;
     }
 
     .avatar {
-        width: 150px; /* Increased width */
-        height: 150px; /* Increased height */
-        margin: 0 auto; /* Centering the image */
+        width: 150px;
+        height: 150px;
+        margin: 0 auto;
     }
 
     .card-body {
         color: black;
-    }
-
-    .card-body h2 {
-        text-align: center;
-        font-size: large;
-        font-weight: 600;
     }
 
     .icons {
@@ -79,11 +84,12 @@
     }
 
     .icon {
-        width: 24px; /* Adjust the size as needed */
+        width: 24px;
         height: 24px;
-        fill: white; /* Color of the icons */
+        fill: white;
     }
 </style>
+
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css">
@@ -92,7 +98,6 @@
     {#if showDetails}
         <div class="details-container active">
             <h2>{member?.name}</h2>
-
             <div class="icons">
                 {#if member?.website}
                     <a href="{member?.website}">
@@ -120,24 +125,20 @@
                     </a>
                 {/if}
             </div>
-            
-
             <p>{member?.about}</p>
         </div>
-    {:else}
-        <figure class="flex-col bg-base-100" class:hidden={showDetails}>
-            <div class="avatar inline-flex place-content-center place-items-start rounded-full bg-gradient-to-r from-cyan-500 to-blue-500">
-                <img src={member?.image} alt="{`${member?.name} profile image`}" class="w-full h-auto rounded-full" />
-            </div>
-        </figure>
-
-        <div class="card-body">
-            <h2>
-                {member?.name}
-            </h2>
-            <p>
-                {member?.title}
-            </p>
-        </div>
     {/if}
+    <figure class="flex-col bg-base-100">
+        <div class="avatar inline-flex place-content-center place-items-start rounded-full bg-gradient-to-r from-cyan-500 to-blue-500">
+            <img src={member?.image} alt="{`${member?.name} profile image`}" class="w-full h-auto rounded-full" />
+        </div>
+    </figure>
+    <div class="card-body">
+        <h2>
+            {member?.name}
+        </h2>
+        <p>
+            {member?.title}
+        </p>
+    </div>
 </div>
