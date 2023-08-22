d761edceebe069d8aa105a430f6fbd5969abda98
yilunzhao <yilun.zhao@yale.edu>
2023-08-22 01:22:37 -0400

    finish the draft version of main page

--- src/lib/Layout/Hero/ImageHero.svelte
+++ src/lib/Layout/Hero/ImageHero.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
     export let backgroundImage: string;
-    export let subtitle:string;
 
 </script>
 
-<div class="hero h-[20rem] bg-base-200 {$$props.class}" style="background-image: url({backgroundImage});">
+<div class="hero h-[15rem] bg-base-200 {$$props.class}" style="background-image: url({backgroundImage});">
   {#if backgroundImage}
         <div class="hero-overlay bg-opacity-60"></div>
   {/if}   
   
   <div class="hero-content text-center text-neutral-content">
     <div class="max-w-md">
-      <h1 class="mb-5 text-5xl md:text-7xl font-light capitalize break-all">
-          <slot>
-          </slot>
-      </h1>              
-      {#if subtitle}
-        <p class="mb-5 text-2xl uppercase font-extralight break-keep">
-          {@html subtitle}
-        </p>
-      {/if}  
+      <h1 class="mb-5 text-white md:text-xl font-light break-all">
+        <slot>
+        </slot>
+      </h1>      
       <slot name="tagline"></slot>        
     </div>
   </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,51 +36,142 @@
 </script>
 
 <style>
-    .content-container {
-        display: flex;
-    }
-
     .main-content {
-        flex: 1;
+        display: flex;
+        justify-content: space-between;
     }
 
     .twitter-feed {
         width: 30%;  /* Adjusted to 25% for more compact appearance */
         min-width: 250px; /* Adjusted to a smaller minimum width */
-        height: 600px;  /* Fixed height for the window effect */
+        height: 410px;  /* Fixed height for the window effect */
         overflow-y: auto;  /* Allow internal vertical scrolling */
         border: 1px solid #ccc;  /* Optional border styling */
         box-shadow: 0 0 10px rgba(0,0,0,0.1);  /* Optional shadow for a "lifted" effect */
     }
+
+    .line-through-title {
+        position: relative;
+        text-align: center;
+        margin-bottom: 15px;
+    }
+
+    .line-through-title span {
+        background-color: #fff; /* Assuming your background is white. If not, change this. */
+        padding: 0 10px; /* Adjust as needed to give space around the text. */
+        z-index: 1;
+        position: relative;
+        font-size: xx-large;
+    }
+
+    .line-through-title::before {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 0;
+        right: 0;
+        height: 1px;
+        background: black; /* Adjust the color as needed. */
+        z-index: 0;
+    }
+
+    .news-section {
+        width: 65%;
+        min-width: 250px;
+        height: 450px; 
+        overflow-y: auto; 
+        color: #5d5c5c;
+        margin-top: -20px;
+    }
+    .news-list {
+        list-style-type: none; /* No bullets */
+        padding: 0;
+    }
+
+    .news-item {
+        margin-bottom: 5px;
+    }
+
+    .news-date {
+        font-weight: bold;
+        padding: 2px 6px;
+    }
+
 </style>
 
 <Hero class="hidden md:block"></Hero>
+<Hero backgroundImage="/images/projects_hero.jpeg">
+    Lab description here
+</Hero>
 
-<div class="content-container">
-
-    <!-- Main Content -->
+<JellyContainer class="pt-16">
     <div class="main-content">
-        <div class="hidden md:flex flex-row">    
+        <!-- <div class="hidden md:flex flex-row">    
             <div class="w h-[36rem]">
                 <Carousel            
                     images={myImages}
                     showCaptions={false}             
                 />
             </div>
+        </div> -->
+
+        <div class="twitter-feed">
+            <a class="twitter-timeline" href="https://twitter.com/yalenlp?ref_src=twsrc%5Etfw">Tweets by yalenlp</a>
         </div>
 
-        <JellyContainer>
-            <div class="flex place-content-center md:pt-8">
-                <div class="text-slate-700 leading-relaxed">
-                    Lab description here
-                </div>
+        <div class="news-section">
+            <div class="line-through-title">
+                <span>News</span>
             </div>
-        </JellyContainer>
+            <ul class="news-list" style="overflow-x:auto; height: 380px">
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+                <li class="news-item">
+                    <span class="news-date">2023/5</span> x papers presented at ACL 2023.
+                </li>
+            </ul>
+        </div>
+
     </div>
 
-    <!-- Twitter Feed Section -->
-    <div class="twitter-feed">
-        <a class="twitter-timeline" href="https://twitter.com/yalenlp?ref_src=twsrc%5Etfw">Tweets by yalenlp</a>
-    </div>
-
-</div>
+</JellyContainer>
